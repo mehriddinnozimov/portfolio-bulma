06926e02f8d20385278cc37961f8ebb17240e37f
Mehriddin Nozimov <mehriddin0507@gmail.com>
2022-07-26 23:25:51 +0500

    v1.1

--- js/index.js
+++ js/index.js
@@ -26,3 +26,27 @@ const fix_navbar = document.getElementById("fix-navbar")
 const navbar = document.getElementById("navbar")
 fix_navbar.style.height = getComputedStyle(navbar).height
 fix_navbar.style.content = ""
+
+const navbar_menu = document.getElementById("navbarBasicExample")
+const navbar_items = document.getElementsByClassName("navbar-item")
+
+const navbar_button = document.getElementById("navbar-button")
+navbar_button.addEventListener("click", () => {
+	navbar_menu.classList.toggle("bg-half-dark")
+	for(let i = 0; i < navbar_items.length; i++) {
+		navbar_item = navbar_items[i]
+		navbar_item.classList.toggle("navbar-primary")
+	}
+
+	navbar_button.classList.toggle("is-active")
+	navbar_menu.classList.toggle("is-active")
+})
+
+const cards = document.getElementsByClassName("tech-card")
+
+for(let i = 0; i < cards.length; i++){
+	const star = cards[i].children[0].children[0]
+	star.addEventListener("mouseleave", () => {
+		star.classList.toggle("out")
+	})
+}
